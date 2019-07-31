a56675a79bf83ae23deb985ed1401bf4ac5f7d53
avdhutw <avdhutwakchaure@gmail.com>
2019-07-31 11:44:51 +0530

    Register user and plan controller

---
+++ Controllers/Register_PlanController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using demoofuserplans.Models;
+
+namespace demoofuserplans.Controllers
+{
+    public class Register_PlanController : Controller
+    {
+        private DemochatbootsEntities1 db = new DemochatbootsEntities1();
+
+        // GET: Register_Plan
+        public ActionResult Index()
+        {
+            return View(db.Prepaid_Plan.ToList());
+        }
+
+        // GET: Register_Plan/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Prepaid_Plan prepaid_Plan = db.Prepaid_Plan.Find(id);
+            if (prepaid_Plan == null)
+            {
+                return HttpNotFound();
+            }
+            return View(prepaid_Plan);
+        }
+
+        // GET: Register_Plan/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Register_Plan/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Plan_id,Plan_name,Noof_day,Datalimit_per,Noof_sms,Noof_calls,Isunlimitedcalling_enabled")] Prepaid_Plan prepaid_Plan)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Prepaid_Plan.Add(prepaid_Plan);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(prepaid_Plan);
+        }
+
+        // GET: Register_Plan/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Prepaid_Plan prepaid_Plan = db.Prepaid_Plan.Find(id);
+            if (prepaid_Plan == null)
+            {
+                return HttpNotFound();
+            }
+            return View(prepaid_Plan);
+        }
+
+        // POST: Register_Plan/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Plan_id,Plan_name,Noof_day,Datalimit_per,Noof_sms,Noof_calls,Isunlimitedcalling_enabled")] Prepaid_Plan prepaid_Plan)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(prepaid_Plan).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(prepaid_Plan);
+        }
+
+        // GET: Register_Plan/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Prepaid_Plan prepaid_Plan = db.Prepaid_Plan.Find(id);
+            if (prepaid_Plan == null)
+            {
+                return HttpNotFound();
+            }
+            return View(prepaid_Plan);
+        }
+
+        // POST: Register_Plan/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Prepaid_Plan prepaid_Plan = db.Prepaid_Plan.Find(id);
+            db.Prepaid_Plan.Remove(prepaid_Plan);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ Controllers/Register_UserController.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using demoofuserplans.Models;
+
+namespace demoofuserplans.Controllers
+{
+    public class Register_UserController : Controller
+    {
+        private DemochatbootsEntities1 db = new DemochatbootsEntities1();
+
+        // GET: Register_User
+        public ActionResult Index()
+        {
+            return View(db.Users.ToList());
+        }
+
+        // GET: Register_User/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            User user = db.Users.Find(id);
+            if (user == null)
+            {
+                return HttpNotFound();
+            }
+            return View(user);
+        }
+
+        // GET: Register_User/Create
+        public ActionResult Create()
+        {
+            ViewBag.plans = new SelectList(db.Prepaid_Plan, "Plan_id", "Plan_name");
+
+
+            return View();
+        }
+
+        // POST: Register_User/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Client_id,Phone_no,First_name,Last_name,Current_plan,Lastthree_m,Lastsix_m,Lastone_yr,Typeof_user,DOB,Plan_id")] User user)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Users.Add(user);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(user);
+        }
+
+        // GET: Register_User/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            ViewBag.plans = new SelectList(db.Prepaid_Plan, "Plan_id", "Plan_name");
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            User user = db.Users.Find(id);
+            if (user == null)
+            {
+                return HttpNotFound();
+            }
+            return View(user);
+        }
+
+        // POST: Register_User/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Client_id,Phone_no,First_name,Last_name,Current_plan,Lastthree_m,Lastsix_m,Lastone_yr,Typeof_user,DOB,Plan_id")] User user)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(user).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(user);
+        }
+
+        // GET: Register_User/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            User user = db.Users.Find(id);
+            if (user == null)
+            {
+                return HttpNotFound();
+            }
+            return View(user);
+        }
+
+        // POST: Register_User/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            User user = db.Users.Find(id);
+            db.Users.Remove(user);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
